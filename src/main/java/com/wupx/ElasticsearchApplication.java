3eb5c73275798828a4be6f88db8feac8b87917d7
wukefan <363153421@qq.com>
2023-11-10 09:35:37 +0800

    Initial commit

---
+++ src/main/java/com/wupx/ElasticsearchApplication.java
@@ -0,0 +1,17 @@
+package com.wupx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wuKeFan
+ * @date 2023/07/01
+ */
+@SpringBootApplication
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wupx/constant/Constant.java
@@ -0,0 +1,9 @@
+package com.wupx.constant;
+
+/**
+ * @author wuKeFan
+ * @date 2023/07/02
+ */
+public interface Constant {
+    String INDEX = "user";
+}
